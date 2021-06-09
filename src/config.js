ad8085747ec3bb878adc1eebd8a52cbae1f25f1f
Sreeram Padmanabhan <sreeram.com@gmail.com>
2021-06-09 15:40:11 +0200

    chore: config

--- src/config.js
+++ src/config.js
@@ -75,16 +75,16 @@ module.exports = {
         { title: 'Next big step in life', route: '/story/next-big-step' }, // 02/06/21
         { title: 'My professional journey', route: '/story/my-professional-journey' }, // 23/04/21
         { title: 'Studio for rent', route: '/story/studio-for-rent-berlin' }, // 01/04/21
-        { title: 'My balcony in India', route: '/story/view-from-my-balcony' }, // 14/02/21
+        // { title: 'My balcony in India', route: '/story/view-from-my-balcony' }, // 14/02/21
         { title: 'Life in IN/DE/UK', route: '/story/life-in-de-uk' }, // 30/11/20
         { title: 'The past decade', route: '/story/the-past-decade' }, // 01/12/20
         { title: 'Moving to Germany', route: '/story/moving-to-germany' }, // 09/08/20
-        { title: 'Best photo I\'ve ever taken', route: '/story/best-photo-I\'ve-ever-taken' }, // 05/12/18
-        { title: 'View from my desk', route: '/story/view-from-my-desk' }, // 19/11/18
+        // { title: 'Best photo I\'ve ever taken', route: '/story/best-photo-I\'ve-ever-taken' }, // 05/12/18
+        // { title: 'View from my desk', route: '/story/view-from-my-desk' }, // 19/11/18
         { title: 'Indians in Britian', route: '/story/indians-in-britain' }, // 01/08/17
-        { title: 'Paragliding at Interlaken', route: '/story/paragliding-at-interlaken' }, // 25/06/17
-        { title: 'Top of Scotland', route: '/story/top-of-scotland' }, // 24/04/17
-        { title: 'Shard from the tower', route: '/story/view-of-the-shard' }, // 28/12/2016
+        // { title: 'Paragliding at Interlaken', route: '/story/paragliding-at-interlaken' }, // 25/06/17
+        // { title: 'Top of Scotland', route: '/story/top-of-scotland' }, // 24/04/17
+        // { title: 'Shard from the tower', route: '/story/view-of-the-shard' }, // 28/12/2016
         // { title: 'How I organize my life', route: '/story/organize' },
       ],
     },
